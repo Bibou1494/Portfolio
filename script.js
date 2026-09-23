14641eddfde263127e81d04f595fb0c68f1fdb01
Bibou1494 <gdmcplayer@proton.me>
2026-09-23 20:42:17 +0200

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const title = document.getElementById("title")
 const artist = document.getElementById("artist")
 
 async function nowplaying() {
-  const response = await fetch("http://127.0.0.1:3000/nowplaying", {
+  const response = await fetch("http://192.168.1.89:3000/nowplaying", {
     method: "GET",
     headers: {
       "Content-Type": "application/json"
