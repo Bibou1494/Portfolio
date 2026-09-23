d208b6b29d51911e212ae93dba188bc0e829b4b3
Bibou1494 <gdmcplayer@proton.me>
2026-09-23 20:36:41 +0200

    Added NowPlaying and redisigned the website

---
+++ script.js
@@ -0,0 +1,24 @@
+const title = document.getElementById("title")
+const artist = document.getElementById("artist")
+
+async function nowplaying() {
+  const response = await fetch("http://127.0.0.1:3000/nowplaying", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    },
+  })
+  if(!response.ok) {
+    throw new Error ("Server offline");
+  }
+
+  const track = await response.json()
+  
+  if(track.nowPlaying) {
+    title.textContent = `Now playing: ${track.name}`;
+    artist.textContent = `By: ${track.artist}`;
+  }
+}
+
+nowplaying();
+setInterval(nowplaying, 30000);
